ad7dc4cc2b1a07c48b1eb2d29a786b9554f72590
Fábio André Zatta <fisiofaz8@gmail.com>
2026-02-06 15:26:31 -0300

    feat: implementa put e delete de materias-primas e correcoes de layout

--- production-control/src/main/java/com/autoflex/production_control/RawMaterialResource.java
+++ production-control/src/main/java/com/autoflex/production_control/RawMaterialResource.java
@@ -23,4 +23,38 @@ public class RawMaterialResource {
         rawMaterial.persist();
         return Response.status(Response.Status.CREATED).entity(rawMaterial).build();
     }
+
+    @PUT
+    @Path("/{id}")
+    @Transactional
+    public Response update(@PathParam("id") Long id, RawMaterial dto) {
+        // Busca no banco
+        RawMaterial entity = RawMaterial.findById(id);
+
+        if (entity == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        // Atualiza os dados
+        entity.name = dto.name;
+        entity.stockQuantity = dto.stockQuantity;
+        
+        // O Panache salva automaticamente ao fim da transação
+        return Response.ok(entity).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Transactional
+    public Response delete(@PathParam("id") Long id) {
+        // 1. Tenta deletar
+        boolean deleted = RawMaterial.deleteById(id);
+        
+        if (!deleted) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+        
+        // 2. Retorna sucesso sem conteúdo (204 No Content)
+        return Response.noContent().build();
+    }
 }
